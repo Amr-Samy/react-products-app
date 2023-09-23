9a5f0b9b3f77bb4e4e40619abfc74bdd4c1e5a15
Amr Samy <36173696+Amr-Samy@users.noreply.github.com>
2023-09-24 00:03:53 +0200

    Updates

--- src/components/api/axios.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-
-export const axiosInstance = axios.create({
-    // apis base url
-    baseURL: 'https://dummyjson.com',
-    // timeout: 1000,
-    // headers: {'X-Custom-Header': 'foobar'}
-  });
